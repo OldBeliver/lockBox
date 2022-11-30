e1ab47796addd9808d5318c2b8de870851f8871a
OldBeliver <380506338004@yandex.com>
2022-11-30 11:14:04 +0300

    добавлено меню

--- Program.cs
+++ Program.cs
@@ -6,6 +6,10 @@ namespace lockBox
     {
         static void Main(string[] args)
         {
+            const string NewGameCommand = "1";
+            const string RulesOfGameCommand = "2";
+            const string ExitCommand = "3";
+
             Random random = new Random();
             int lowerRange = 1;
             int upperRange = 9;
@@ -14,62 +18,94 @@ namespace lockBox
             string userInput = "";
             int size = 4;
             int counter = 0;
+            bool isPlaying = true;
 
-            for (int i = 0; i < size; i++)
+            while (isPlaying)
             {
-                string nextNumber = "";
+                Console.Clear();
+                Console.WriteLine($"игра Взлом пароля");
+                Console.WriteLine($"------++++-------");
+                Console.WriteLine($"{NewGameCommand}. Новая игра");
+                Console.WriteLine($"{RulesOfGameCommand}. Правила игры");
+                Console.WriteLine($"{ExitCommand}. Выход");
 
-                while (lockBox.Contains(nextNumber))
+                switch (Console.ReadLine())
                 {
-                    nextNumber = Convert.ToString(random.Next(lowerRange, upperRange + 1));
-                }
+                    case NewGameCommand:
+                        Console.Clear();
 
-                lockBox += nextNumber;
-            }          
-
-            Console.WriteLine($"двух одинаковых цифр нет,");
-            Console.WriteLine($"+ цифра на своем месте,");
-            Console.WriteLine($"- цифра не на своем месте.");
-            Console.WriteLine($"--------------------------");
-            Console.WriteLine($"Подберите {size}х значный цифровой код:");
-
-            while (lockBox != userInput)
-            {
-                userInput = "";
-
-                while (userInput.Length != 4)
-                {
-                    userInput = Console.ReadLine();
-
-                    if (userInput.Length != 4)
-                        Console.WriteLine($"введите 4 цифры");
-                }
-
-                for (int i = 0; i < size; i++)
-                {
-                    for (int j = 0; j < size; j++)
-                    {
-                        if (lockBox[i] == userInput[j])
+                        for (int i = 0; i < size; i++)
                         {
-                            if (i == j)
+                            string nextNumber = "";
+
+                            while (lockBox.Contains(nextNumber))
                             {
-                                Console.Write($"+");
+                                nextNumber = Convert.ToString(random.Next(lowerRange, upperRange + 1));
                             }
-                            else
-                            {
-                                Console.Write($"-");
-                            }
+
+                            lockBox += nextNumber;
                         }
-                    }
+
+                        Console.WriteLine($"Подберите {size}х значный цифровой код:");
+
+                        while (lockBox != userInput)
+                        {
+                            userInput = "";
+
+                            while (userInput.Length != 4)
+                            {
+                                userInput = Console.ReadLine();
+
+                                if (userInput.Length != 4)
+                                    Console.WriteLine($"введите 4 цифры");
+                            }
+
+                            for (int i = 0; i < size; i++)
+                            {
+                                for (int j = 0; j < size; j++)
+                                {
+                                    if (lockBox[i] == userInput[j])
+                                    {
+                                        if (i == j)
+                                        {
+                                            Console.Write($"+");
+                                        }
+                                        else
+                                        {
+                                            Console.Write($"-");
+                                        }
+                                    }
+                                }
+                            }
+
+                            counter++;
+                            Console.WriteLine();
+                        }
+
+                        Console.WriteLine($"Код взломан: {lockBox}");
+                        Console.WriteLine($"за {counter} попыток");                        
+                        break;
+
+                    case RulesOfGameCommand:
+                        Console.Clear();
+                        Console.WriteLine($"двух одинаковых цифр нет,");
+                        Console.WriteLine($"+ цифра на своем месте,");
+                        Console.WriteLine($"- цифра не на своем месте.");
+                        Console.WriteLine($"если цифры нет ничего не ставится");
+                        Console.WriteLine($"--------------------------");                        
+                        break;
+
+                    case ExitCommand:
+                        isPlaying = false;
+                        break;
+
+                    default:
+                        Console.WriteLine($"Ошибка ввода команды");
+                        break;
                 }
 
-                counter++;
-                Console.WriteLine();
+                Console.ReadKey();
             }
-
-            Console.WriteLine($"Код взломан: {lockBox}");
-            Console.WriteLine($"за {counter} попыток");
-            Console.ReadKey();
         }
     }
 }
