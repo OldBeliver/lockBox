af007066e1e5459c93209d26d05fa21521dd3943
OldBeliver <380506338004@yandex.com>
2022-12-03 12:38:12 +0300

    initial commit

--- Program.cs
+++ Program.cs
@@ -6,70 +6,7 @@ namespace lockBox
     {
         static void Main(string[] args)
         {
-            Random random = new Random();
-            int lowerRange = 1;
-            int upperRange = 9;
-
-            string lockBox = "";
-            string userInput = "";
-            int size = 4;
-            int counter = 0;
-
-            for (int i = 0; i < size; i++)
-            {
-                string nextNumber = "";
-
-                while (lockBox.Contains(nextNumber))
-                {
-                    nextNumber = Convert.ToString(random.Next(lowerRange, upperRange + 1));
-                }
-
-                lockBox += nextNumber;
-            }          
-
-            Console.WriteLine($"двух одинаковых цифр нет,");
-            Console.WriteLine($"+ цифра на своем месте,");
-            Console.WriteLine($"- цифра не на своем месте.");
-            Console.WriteLine($"--------------------------");
-            Console.WriteLine($"Подберите {size}х значный цифровой код:");
-
-            while (lockBox != userInput)
-            {
-                userInput = "";
-
-                while (userInput.Length != 4)
-                {
-                    userInput = Console.ReadLine();
-
-                    if (userInput.Length != 4)
-                        Console.WriteLine($"введите 4 цифры");
-                }
-
-                for (int i = 0; i < size; i++)
-                {
-                    for (int j = 0; j < size; j++)
-                    {
-                        if (lockBox[i] == userInput[j])
-                        {
-                            if (i == j)
-                            {
-                                Console.Write($"+");
-                            }
-                            else
-                            {
-                                Console.Write($"-");
-                            }
-                        }
-                    }
-                }
-
-                counter++;
-                Console.WriteLine();
-            }
-
-            Console.WriteLine($"Код взломан: {lockBox}");
-            Console.WriteLine($"за {counter} попыток");
-            Console.ReadKey();
+            
         }
     }
 }
