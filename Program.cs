9ff103831545fa167bc6eec6a0ea8b2d6b2bd7a5
OldBeliver <380506338004@yandex.com>
2022-12-03 12:57:19 +0300

    метод CreateNumberKit()

--- Program.cs
+++ Program.cs
@@ -6,7 +6,30 @@ namespace lockBox
     {
         static void Main(string[] args)
         {
-            
+            string[] numbersKit = CreateNumberKit();
+
+            for (int i = 0; i < numbersKit.Length; i++)
+            {
+                Console.Write($"{numbersKit[i]} ");
+            }
+        }
+
+        static string[] CreateNumberKit()
+        {
+            int size = 9;
+            string[] numbers = new string[size];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = $"{i + 1}";
+            }
+
+            return numbers;
+        }
+
+        static void CreateColorSet()
+        {
+
         }
     }
 }
